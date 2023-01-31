9a1cb457793241662d9d8d4a6efab50d7ee9add4
Emri Aharon <emriaharon@gmail.com>
2023-01-31 22:35:00 +0200

    finish Navbar and sideDrawer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from 'react-router-dom';
+
 import './App.css';
 
-function App() {
+//screens
+import  HomeScreen from './screens/HomeScreen';
+import CartScreen from './screens/CartScreen';
+import ProductScreen from './screens/ProductScreen';
+
+//components
+import Navbar from './components/Navbar';
+import Backdrop from './components/Backdrop';
+import SideDrawer from './components/SideDrawer';
+
+function App () {
+  const [sideToggle, setSideToggle] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar click={() => setSideToggle(true)}/>
+      <SideDrawer show={sideToggle} click={()=>setSideToggle(false)}/>
+      <Backdrop show={sideToggle} click={()=>setSideToggle(false)}/>
+      <main>
+        <Routes>
+          <Route exact path ='/' element={<HomeScreen/>} />
+          <Route exact path ='/product/:id' element={<ProductScreen/>}/>
+          <Route exact path ='/cart' element={<CartScreen/>}/>
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
