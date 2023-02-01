39f672fc0d1830e39e6eb0060d4648bbbe10069b
Emri Aharon <emriaharon@gmail.com>
2023-02-01 14:44:42 +0200

    implementation actions

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,13 @@
 import './HomeScreen.css'
+// import { useState } from 'react';
+// import {useSelector, useDispatch  } from 'react-redux';
+
+//components
 import Product from '../components/Product';
 
+
 const HomeScreen = ()=>{
+    
     return<div className='homescreen'>
         <h2 className='homescreen__title'>Latest Products</h2>
         
